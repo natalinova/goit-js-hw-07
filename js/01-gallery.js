89c588795c18dfa41e10ff113e38aabbe165cf80
Nataliya Novak <novak.natali@gmail.com>
2022-07-18 03:08:48 +0300

    last version

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+const galleryMarkup = createGallery(galleryItems);
+console.log(galleryMarkup);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGallery(items) {
+    return items.map(({original, preview, description}) => {
+       return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    }).join('')
+}
+gallery.addEventListener('click', onItemGalleryClick);
+function onItemGalleryClick(evt) {
+    evt.preventDefault();
+    const instance = basicLightbox.create(`<img src=${evt.target.dataset.source}>`
+    )
+    instance.show()
+
+    window.addEventListener('keydown', closeModal)
+    function closeModal(evt)
+    {
+ if (evt.code === 'Escape') {instance.close()}}
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,17 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+console.log(gallery);
+const galleryMarkup = createGallery(galleryItems);
+console.log(galleryMarkup);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup)
+
+function createGallery(items) {
+    return items.map(({original, preview, description}) => {
+       return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    }).join('')
+}
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, enableKeyboard: true, docClose:true, loop:true});
